873a69fa60b2a409ae0a4940468fba511abf37e4
zhangguohua <461974918@qq.com>
2018-08-13 15:44:23 +0800

    page content add

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="one-root"> <!-- 应该包含一个根元素 -->
+		<p>Hello <strong>Wrold!</strong></p>
+		<em>{{ authorName }}</em>
+	</div>
+</template>
+
+<script type="text/javascript">
+	export default {
+		name: 'app',
+		data () {
+			return {
+				authorName: 'zgh'
+			}
+		}
+	}
+</script>
+
+<style>
+	p {
+		color: orange;
+	}
+</style>
